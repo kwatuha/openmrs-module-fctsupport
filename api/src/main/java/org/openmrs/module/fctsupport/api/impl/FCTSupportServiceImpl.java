50888b5549b5a065f68ad4896f477943905f1230
Jeremy Keiper <jeremy@openmrs.org>
2013-10-14 14:04:50 +0300

    initial import

---
+++ api/src/main/java/org/openmrs/module/fctsupport/api/impl/FCTSupportServiceImpl.java
@@ -0,0 +1,44 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.fctsupport.api.impl;
+
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.fctsupport.api.FCTSupportService;
+import org.openmrs.module.fctsupport.api.db.FCTSupportDAO;
+
+/**
+ * It is a default implementation of {@link FCTSupportService}.
+ */
+public class FCTSupportServiceImpl extends BaseOpenmrsService implements FCTSupportService {
+	
+	protected final Log log = LogFactory.getLog(this.getClass());
+	
+	private FCTSupportDAO dao;
+	
+	/**
+     * @param dao the dao to set
+     */
+    public void setDao(FCTSupportDAO dao) {
+	    this.dao = dao;
+    }
+    
+    /**
+     * @return the dao
+     */
+    public FCTSupportDAO getDao() {
+	    return dao;
+    }
+}
